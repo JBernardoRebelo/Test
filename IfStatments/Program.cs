3eb7c492e6fbd2add037cd4314f5a0e99bbe822b
JbernardoRebelo <j.bernardo.rebelo@gmail.com>
2019-03-19 19:54:10 +0000

    Added another method to my method project, creation of an ifStatments file

---
+++ IfStatments/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace IfStatments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //def variables
+            bool isMale = true;
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- Methods/Program.cs
+++ Methods/Program.cs
@@ -9,11 +9,29 @@ namespace Methods
         {
             Console.WriteLine("Hello " + name + " you are " + age);
         }
+        //method new //return
+        static int cube (int num)
+        {
+            //cubing num
+            int result = num * num * num;
+            //returns (cubed num(result))
+            return result;
+        }
         static void Main(string[] args)
         {
-            //calling the method
+            int cubedNum = cube(5);
+            //calling the method string
             SayHi("Shrouded", 12);
-            SayHi("Serenity", 69);
+            SayHi("Serenity", 69); 
+
+            //calling the int method
+            //"cube" and printing it
+            Console.WriteLine(cube(5));
+
+            //another way to print this
+            //is to assign a var to the returned value
+            Console.WriteLine(cubedNum);
+
             Console.ReadLine();
         }
     }
