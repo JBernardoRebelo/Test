7a5ad4a46696c3dca01c2c1f11a1abaa8b852553
JbernardoRebelo <j.bernardo.rebelo@gmail.com>
2019-03-19 20:17:09 +0000

    messing around with IfStatments, kind of complex ifStatment using AND, OR operators, also negating stuff

--- IfStatments/Program.cs
+++ IfStatments/Program.cs
@@ -7,10 +7,36 @@ namespace IfStatments
         static void Main(string[] args)
         {
             //def variables
-            bool isMale = true;
+            bool isGay = true;
+            bool isTall = true;
 
-
-            Console.WriteLine("Hello World!");
+            //is male AND tall "you are gay"
+            //both vars MUST be true
+            if (isGay && isTall)
+            {
+                //isMale is true so print gay
+                Console.WriteLine("You are a tall gay");
+            }
+            //is male OR tall
+            //one of them needs to be true
+            else if (isGay || isTall)
+            {
+                //isMale is false so print question
+                Console.WriteLine("Why are you gay or tall gay");
+            }
+            //NOT tall == isTall is false
+            else if (isGay && !isTall)
+            {
+                //isMale is false so print question
+                Console.WriteLine("Why are you short gay");
+            }
+            //if no var is true
+            else
+            {
+                //basically if everything is false
+                //the guy is straight and short
+                Console.WriteLine("straight short");
+            }
         }
     }
 }
