ddd7fd2906a8ee6967346d899e95dfa1830d668a
JbernardoRebelo <j.bernardo.rebelo@gmail.com>
2019-04-02 22:04:54 +0100

    Added a do while to my while program - to practice - can see values as they're being altered and after the loops -- great for learning - 0 practical use -- Great! 8)

--- WhileLoops/Program.cs
+++ WhileLoops/Program.cs
@@ -7,17 +7,43 @@ namespace WhileLoops
         static void Main(string[] args)
         {
             //declare variables
-            int index = 1;
+            int indexWhile = 1;
+            int indexdoWhi = 12;
 
             //checks condition
             //as long as condition is true run code
-            while (index <= 5)
+            while (indexWhile <= 5)
             {
                 //prints index
-                Console.WriteLine(index);
+                Console.WriteLine(indexWhile);
                 //increments index after output and re-checks
-                index++;
+                indexWhile++;
+                //when while equals false breaks loop
             }
+
+            //markers!!
+            Console.WriteLine("End of while loop");
+            Console.WriteLine("---------//-------");
+            
+            //do while
+            //executes code in brackets
+            //before while condition
+            do
+            {
+                Console.WriteLine(indexdoWhi);
+                indexdoWhi++;
+            } while (indexdoWhi <= 5);
+
+            //markers!!
+            Console.WriteLine("End of do while loop");
+            Console.WriteLine("---------//-------");
+
+            //prints values of variables
+            //to see the changes of the different
+            //types of loops
+            Console.WriteLine("Values after loops:");
+            Console.WriteLine(indexWhile);
+            Console.WriteLine(indexdoWhi);
         }
     }
 }
