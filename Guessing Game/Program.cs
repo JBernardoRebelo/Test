360ce4d0d27e60e1e4c2ad0fd8f78d20e8bd5d2b
JbernardoRebelo <j.bernardo.rebelo@gmail.com>
2019-04-03 19:41:42 +0100

    Made the guessing game a litle more complicated, it now has a do-while in a method being called by the main method

--- Guessing Game/Program.cs
+++ Guessing Game/Program.cs
@@ -4,6 +4,26 @@ namespace Guessing_Game
 {
     class Program
     {
+        static void TheDo()
+        {
+            //declare variables
+            string The2ndWord = "serenity";
+            string guessToo = "";
+
+            //asks for a 2nd word
+            //runs code until user guesses it
+            //yet again
+            do
+            {
+                //asks for another word
+                Console.Write("Guess another word: ");
+                //stores input
+                guessToo = Console.ReadLine();
+            } while (guessToo != The2ndWord);//the condition
+
+            Console.WriteLine("You guessed yet again!");
+
+        }
         static void Main(string[] args)
         {
             //declare variables
@@ -17,12 +37,15 @@ namespace Guessing_Game
             {
                 //asks guess to user
                 Console.Write("Enter guess: ");
+                //stores input
                 guess = Console.ReadLine();
             }
 
             //success message
             Console.WriteLine("You guessed it!");
 
+            //calls 2nd guess method
+            TheDo();
         }
     }
 }
