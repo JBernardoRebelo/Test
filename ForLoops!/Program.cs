b9db5e345ab7ad3c675e1462e7f80a01862096c4
JbernardoRebelo <j.bernardo.rebelo@gmail.com>
2019-04-03 21:58:03 +0100

    Creation of a for loop to print out elements of an array, simple but fun stuff

---
+++ ForLoops!/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ForLoops_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare an array
+            int[] NewArray = { 69, 12, 420, 80085, 13, 68 };
+
+            //runs all elements of the array
+            for (int i = 0; i < NewArray.Length; i++)//"<=" will output error
+            {
+                //prints elements of the array
+                Console.WriteLine(NewArray[i]);//dont forget the "[i]"or it
+                                               //wont know the index of the array
+            }
+            
+        }
+    }
+}
