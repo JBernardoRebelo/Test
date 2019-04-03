27916bd48e1b6390d794318a70ad51c04dcef3d9
JbernardoRebelo <j.bernardo.rebelo@gmail.com>
2019-04-03 19:33:53 +0100

    Made the program, very simple while loop does it

--- Guessing Game/Program.cs
+++ Guessing Game/Program.cs
@@ -6,7 +6,23 @@ namespace Guessing_Game
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //declare variables
+            string TheWord = "shrouded";
+            string guess = "";
+
+            //as long as guess
+            //is different from THE WORD
+            //this code will run
+            while (guess != TheWord)
+            {
+                //asks guess to user
+                Console.Write("Enter guess: ");
+                guess = Console.ReadLine();
+            }
+
+            //success message
+            Console.WriteLine("You guessed it!");
+
         }
     }
 }
