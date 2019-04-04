5ad338557bc909b55e9f3caa3490e835de3ff6c5
JbernardoRebelo <j.bernardo.rebelo@gmail.com>
2019-04-04 12:25:04 +0100

    Learning about classes and objects... 2 elder scrolls' books in there, just the basics of it

---
+++ BasicClassesAndObjects/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicClassesAndObjects
+{
+    class Book
+    {
+        //class atributes
+        //describe what is the "Book"
+        public string title;
+        public string author;
+        public int pages;
+    }
+}
---
+++ BasicClassesAndObjects/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace BasicClassesAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //class tells us what the data type is
+            //object is an instance of that class
+            //creating an object from the class "Book"
+            Book book1 = new Book();
+
+            //creation of book 2
+            Book book2 = new Book();
+
+            //assigning the book1 information
+            book1.title = "Absurdity has become necessity";
+            book1.author = "Anonymous";
+            book1.pages = 4;
+
+            //book2 information
+            book2.title = "Morning Star v.1";
+            book2.author = "Some High Elf";
+            book2.pages = 10;
+
+            //print atributes of the book
+            Console.WriteLine("Book 1");
+            Console.WriteLine("Title: " + book1.title);
+            Console.WriteLine("Author: " + book1.author);
+            Console.WriteLine("Pages: " + book1.pages);
+
+            //print atributes of 2nd book
+            Console.WriteLine("Book 2");
+            Console.WriteLine("Title: " + book2.title);
+            Console.WriteLine("Author: " + book2.author);
+            Console.WriteLine("Pages: " + book2.pages);
+
+        }
+    }
+}
