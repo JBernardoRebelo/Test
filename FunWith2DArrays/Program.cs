4c3acff63bf5390248eb404fa95217303b630137
JbernardoRebelo <j.bernardo.rebelo@gmail.com>
2019-04-04 11:28:33 +0100

    Seeing the 2D arrays work as simple as possible

---
+++ FunWith2DArrays/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FunWith2DArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creation of a bidimensional array
+            //every element must be an array
+            int[,] nGrid =
+            {
+                {1,2},
+                {3,4},
+                {5,6}
+            };
+
+            //another 2D array but
+            //NO SPECIFICATION of the elements inside
+            //only the sioze of it 
+            int[,] newArray = new int[2, 3];
+
+            //prints out elements of array
+            //by index position choice
+            Console.WriteLine(nGrid[1,1]);
+        }
+    }
+}
