b7285b14ef5d6769d2082090412e26a6bcb81783
JBernardoRebelo <bernasfattaker@gmail.com>
2019-04-10 17:16:46 +0100

    Test programs as usual - rpg classes - turned based game

---
+++ RPGclasses/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace RPGclasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // EXERCISE 36
+
+            // Declares variables
+            string choice;
+            // Declaring variables classes
+            string warrior = "Melee hack-and-slash class.";
+            string knight = "Heavy armor class.";
+            string barbarian = "Big guy with an equally big weapon.";
+            string monk = "Melee class that forgoes weapons for their powerful fists.";
+            string archer = "Ranged class with bows or guns.";
+            string thief = "Sneaky bandit with itchy fingers and a nose for treasure.";
+            string priest = "Class who makes sure the party survives an encounter.";
+            string mage = "Class with spells to burn, freeze or electrify.";
+            string druid = "Class capable of summoning minions.";
+
+            // Welcome's user and explains "game"
+            Console.WriteLine("Welcome to choose your class");
+            Console.WriteLine("You can pick one from these classes:");
+            // Showing classes available
+            Console.WriteLine("1 - Warrior");
+            Console.WriteLine("2 - Knight");
+            Console.WriteLine("3 - Barbarian");
+            Console.WriteLine("4 - Monk");
+            Console.WriteLine("5 - Archer");
+            Console.WriteLine("6 - Thief");
+            Console.WriteLine("7 - Priest");
+            Console.WriteLine("8 - Mage");
+            Console.WriteLine("9 - Druid");
+
+            // Ask user 
+            Console.Write("Choose your class: ");
+            choice = Console.ReadLine();
+
+            // Checks choice
+            switch (choice)
+            {
+                // Warrior
+                case "1":
+                    Console.WriteLine(warrior);
+                    break;
+                // Knight
+                case "2":
+                    Console.WriteLine(knight);
+                    break;
+                // Barbarian
+                case "3":
+                    Console.WriteLine(barbarian);
+                    break;
+                // Monk
+                case "4":
+                    Console.WriteLine(monk);
+                    break;
+                // Archer
+                case "5":
+                    Console.WriteLine(archer);
+                    break;
+                // Thief
+                case "6":
+                    Console.WriteLine(thief);
+                    break;
+                // Priest
+                case "7":
+                    Console.WriteLine(priest);
+                    break;
+                // Mage
+                case "8":
+                    Console.WriteLine(mage);
+                    break;
+                // Druid
+                case "9":
+                    Console.WriteLine(druid);
+                    break;
+                default:
+                    Console.WriteLine("Invalid input!");
+                    break;
+            }
+        }
+    }
+}
---
+++ Turned-BaseIllusion/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Turned_BaseIllusion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare variables
+            int maxturns;
+            int turns = 1;
+            bool live = true;
+            double chance;
+            // Creating the Random object
+            Random rng = new Random();
+
+            // Dodging exceptions and erros
+            // If code wasn't run through console
+            // It would throw an exception -- fixing that
+            try
+            {
+                // Converts console's first argument to int
+                maxturns = Convert.ToInt32(args[0]);
+
+                // This wouldn't work if a string was entered
+                // On the console, so...
+            }
+            catch
+            {
+                // If program crashes
+                // Asks user for input
+                // And proceeds program
+                Console.Write("Enter max turns: ");
+                maxturns = Convert.ToInt32(Console.ReadLine());
+            }
+
+            // Greets user
+            Console.WriteLine("Hello player, this is NOT");
+            Console.WriteLine("an interactive program");
+
+            // Reinforces number of maxturns and starts "game"
+            Console.WriteLine("You have too survive " + maxturns);
+            Console.WriteLine("You only have 1 life... ");
+            
+            // As long as the number of lives is 1 and maxturns
+            // And maxturns asn't been reached run:
+            while (live = true && turns <= maxturns)
+            {
+                // Probability/alive check
+                chance = rng.NextDouble();
+
+                // Each turn player has chance to die of 2%
+                if (chance < 0.02)
+                {
+                    live = false;
+
+                    // Die message
+                    Console.WriteLine("Player has died after #" + turns + " Turns");
+                    break;
+                }
+
+                // Status message while alive
+                Console.WriteLine("Player has survided Turn #" + turns);
+
+                if (turns == maxturns)
+                {
+                    // Status message when player survives all turns
+                    Console.WriteLine("Player survived after #" + turns + " Turns");
+                    break;
+                }
+
+                // Increments number of turns
+                turns++;   
+                
+                // End of while
+            }
+        }
+    }
+}
