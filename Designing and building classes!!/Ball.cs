1728f0e650a19ae535dfc968f9345bd21c89c0cf
JBernardoRebelo <j.bernardo.rebelo@gmail.com>
2019-04-17 18:31:50 +0100

    Deleted test from main and instatiated new balls and colors, reworked balls and colors classes

--- Designing and building classes!!/Ball.cs
+++ Designing and building classes!!/Ball.cs
@@ -7,82 +7,48 @@ namespace Designing_and_building_classes__
     class Ball
     {
         // Declare variables
-        private int radius;
+        private float radius;
         private double vSize;
-        private int diameter;
-        private static int totalBalls = 0;
         private int throwCount = 0;
+        // Calling the colors
+        private Color color; 
 
         // Constructor 
-        public Ball (int radius, int throwCount)
+        public Ball (int radius, Color color)
         {
+
+            // Seting the color
+            this.color = color;
             // Assigning a value to radius
-            this.radius = radius;
+            //this.vSize = 4 / 3 * Math.PI * radius; 
 
             // Defining in the constructor what each variable does
             vSize = 4 / 3 * Math.PI * radius;
-            diameter = radius * 2;
 
             // Increases number of balls everytime it gets constructed
-            totalBalls++;
             throwCount++;
         }
 
-        // Methods
-
-        // Gets
-        // Readsonly -- returns volume (size)
-        public double GetvSize()
-        {
-             return vSize;
-        }
-
-        // Returns diameter
-        public int Getdiameter()
-        {
-            return diameter;
-        }
+        // Returns volume (size)
+        public double GetvSize() => vSize;
 
         // Returns radius
-        public int GetRadius()
-        {
-            return radius;
-        }
+        public float GetRadius() => radius;
 
-        // Gets number of balls instatiated
-        public int GettotalBalls()
-        {
-            return totalBalls;
-        }
-
-        public int GetthrownBalls()
-        {
-            return throwCount;
-        }
+        public int GetthrownBalls() => throwCount;
 
         // SET
         // Changing radius value if needed
-        public void SetRadius(int radius)
-        {
-            this.radius = radius;
-        }
+        public void SetRadius(int radius) => this.radius = radius;
 
         // Sets size of ball to 0
-        public void PopSize(double vSize)
-        {
-            vSize = 0;
-        }
+        public void PopSize(double vSize) => vSize = 0;
 
         // Throw a ball!
-        public void Throw(int i)
+        public void Throw()
         {   
-            // Incrementing throw count until it reaches required number
-            while (i > throwCount )
-            {
-                throwCount++;
-            }
-            // If the user chooses to throw the balls again only goes +1 up
-            if (i == 1)
+            // If the ball hasn't been popped throws!
+            if (vSize > 0)
             {
                 throwCount++;
             }
--- Designing and building classes!!/Color.cs
+++ Designing and building classes!!/Color.cs
@@ -6,69 +6,42 @@ namespace Designing_and_building_classes__
 {
     class Color
     {
-        // Declare variables -- colors
+        // Declare variables
         // ALL COLORS MAX VALUE == 255
-        private int red;
-        private int green;
-        private int blue;
-        private int alpha;
-        private int maxval = 255;
+        private byte red;
+        private byte green;
+        private byte blue;
+        private byte alpha;
         private float grayscale;
-
-        // Making a constructor
-        public Color(int red, int green, int blue, int alpha)
+        
+        // Making a constructor that sets alpha to max
+        public Color(byte red, byte green, byte blue, byte alpha)
         {
             // Defines color values
             this.red = red;
             this.green = green;
             this.blue = blue;
-
-            // Alpha sets to default value
-            alpha = maxval;
-
-            // If values entered pass maxval do this
-            if (red > maxval)
-            {
-                // If user types more than maxval assigne it's value
-                Console.WriteLine("Red has been set to 255.");
-                red = maxval;
-
-            }
-            else if (green > maxval)
-            {
-                // If user types more than maxval assigne it's value
-                Console.WriteLine("Green has been set to 255.");
-                green = maxval;
-            }
-            else if(blue > maxval)
-            {
-                // If user types more than maxval assigne it's value
-                Console.WriteLine("Blue has been set to 255.");
-                blue = maxval;
-            }
-
+            this.alpha = alpha;
         }
-
+        // Opaque color
+        public Color(byte red, byte green, byte blue)
+        {
+            // Defines color values
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+            alpha = 255;
+        }
         // GET AND SET METHODS START HERE
 
-        // READONLY GETS
         // Returns red value
-        public int GetRed()
-        {
-            return red;
-        }
+        public int GetRed() => red;
 
         // Returns green value
-        public int GetGreen()
-        {
-            return green;
-        }
+        public int GetGreen() => green;
 
         // Returns blue value
-        public int GetBlue()
-        {
-            return blue;
-        }
+        public int GetBlue() => blue;
 
         // Gets grayscale -- returns average of all colors
         public float GetGrayscale()
@@ -77,24 +50,13 @@ namespace Designing_and_building_classes__
             grayscale = (red + green + blue) / 3;
             return grayscale;
         }
-
-        // ASSIGNONLY SETS -- doens't return values (void)
         // Set red value
-        public void SetRed(int red)
-        {
-            this.red = red;
-        }
+        public void SetRed(byte red) => this.red = red;
 
         // Set green value
-        public void SetGreen(int green)
-        {
-            this.green = green;
-        }
+        public void SetGreen(byte green) => this.green = green;
 
         // Set blue value
-        public void SetBlue(int blue)
-        {
-            this.blue = blue;
-        }
+        public void SetBlue(byte blue) => this.blue = blue;
     }
 }
